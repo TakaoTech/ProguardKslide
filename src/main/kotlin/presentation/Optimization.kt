cebae27c1dc1bf583ce52baa1e8f7358a3a35464
samuele794 <samuelebruschi794@gmail.com>
2024-01-02 23:06:21 +0100

    Update Presentation

--- src/main/kotlin/presentation/Optimization.kt
+++ src/main/kotlin/presentation/Optimization.kt
@@ -45,4 +45,102 @@ fun Presentation.optimizationConfigPage() {
 		}
 	}
 
+	markdownSlide {
+		slideConfig {
+			// Assign slide config defaults for all slides in this presentation
+			transition = Transition.ZOOM
+		}
+		content {
+			"""
+				```
+				-optimizationpasses &lt;numero&gt;
+				```
+		
+				Notes:
+				Specifica a ProGuard il numero di passaggi di ottimizzazione che devono essere eseguiti.
+			""".trimIndent()
+		}
+	}
+
+	markdownSlide {
+		slideConfig {
+			// Assign slide config defaults for all slides in this presentation
+			transition = Transition.ZOOM
+		}
+		content {
+			"""
+				```
+				-assumenosideeffects [specifica classe]
+				
+				-assumenosideeffects class android.util.Log {    
+					public static boolean isLoggable(java.lang.String, int);    
+					public static int v(...);    
+					public static int i(...);    
+					public static int w(...);   
+					public static int d(...);    
+					public static int e(...);
+				}
+				```
+		
+				Notes:
+				Specifica i metodi che non hanno nessun effetto collaterale, oltre alla possibilità di restituire un valore.
+				Nella fase di ottimizzazione ProGuard rimuoverà tali metodi se determina che i valori restituiti non vengano usati.
+				Usare con cautela, potrebbe rompere il codice.
+
+			""".trimIndent()
+		}
+	}
+
+	markdownSlide {
+		slideConfig {
+			// Assign slide config defaults for all slides in this presentation
+			transition = Transition.ZOOM
+		}
+		content {
+			"""
+				```
+				-assumevalues [specifica classe/i]
+				
+				-assumevalues class Count {  
+					static int count return 0..2147483647;
+				}
+				
+				if (count/* [0..2147483647] */ < 0) {  
+					throw new IllegalStateException();
+				}
+				
+				
+				if (false) {  
+					throw new IllegalStateException();
+				}
+				```
+		
+				Notes:
+				Specifica dei valori fissi o intervalli di valori per i campi e metodi primitivi, 
+				adattando l'ottimizzazione di ProGuard su quei valori.
+				
+			""".trimIndent()
+		}
+	}
+
+	markdownSlide {
+		slideConfig {
+			// Assign slide config defaults for all slides in this presentation
+			transition = Transition.ZOOM
+		}
+		content {
+			"""
+				```
+				-allowaccessmodification
+
+				```
+		
+				Notes:
+				Specifica a ProGuard che i modificatori di accesso alle classi e membri della classe possono essere ampliati, ad esempio con un getter pubblico potrebbe essere necessario rendere pubblica anche il campo.
+				Nota: non usare sulle librerie
+				
+			""".trimIndent()
+		}
+	}
+
 }
