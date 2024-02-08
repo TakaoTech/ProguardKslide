9815187e95af22c76b53873618721101b985c619
samuele794 <samuelebruschi794@gmail.com>
2024-02-08 20:26:06 +0100

    WIP Add Linkedin IFRAME

--- src/main/kotlin/presentation/StartPage.kt
+++ src/main/kotlin/presentation/StartPage.kt
@@ -3,5 +3,13 @@ package presentation
 import com.kslides.Presentation
 
 fun Presentation.startPage() {
+	dslSlide {
+		slideConfig {
+			backgroundIframe = "https://www.linkedin.com/in/samuele794"
+		}
 
+		content {
+
+		}
+	}
 }
